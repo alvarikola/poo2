ee47f5244eb55f64da3d75015bced2eddbb1c85f
alvarikola <alvaroe484@gmail.com>
2024-03-05 13:09:34 +0100

    arreglado days_in_month

--- date.py
+++ date.py
@@ -28,9 +28,9 @@ class Date:
 
         #Otra opción es:  if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     @staticmethod
-    def days_in_month(month: int, year: int) -> int:
+    def days_in_month( month: int, year: int) -> int:
         days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if self.is_leap_year(year) and month == 2:
+        if Date.is_leap_year(year) and month == 2:
             return 29
         else:
             return days[month - 1]
--- probardate.py
+++ probardate.py
@@ -2,3 +2,4 @@ from date import Date
 
 unDia = Date(4, 3, 2024)
 print(unDia.is_leap_year(2024))
+print(unDia.days_in_month(2, 2024))
