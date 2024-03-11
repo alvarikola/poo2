8049fd9ea32505d9e77e7edd01e3593ca8a19d76
alvarikola <alvaroe484@gmail.com>
2024-03-11 12:59:53 +0100

    corregido transporte

--- transporte.py
+++ transporte.py
@@ -2,11 +2,9 @@ class Transporte:
     def __init__(self, *, ruedas:int, asientos:int):
         self.ruedas = ruedas
         self.asientos = asientos
-        # self.x = 0
-        # self.y = 0
 
 
-    def desplazar(x:int, y:int = 0):
+    def desplazar(self, x:int, y:int = 0):
         print(f"El transporte se mueve {x} en horizontal y {y} en vertical")
         
 
@@ -14,13 +12,3 @@ class Transporte:
     def informacion(self):
         print(f"El transporte tiene {self.ruedas} ruedas y {self.asientos} asientos")
 
-
-
-def  main():
-    coche = Transporte(4, 5)
-    coche.desplazar(10, 34)
-    coche.informacion(4, 5)
-    
-
-
-main()
