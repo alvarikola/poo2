0ee66207570dedafc6526444e812c92e59805405
alvarikola <alvaroe484@gmail.com>
2024-03-12 11:26:48 +0100

    creado vehiculo.py

---
+++ vehiculo.py
@@ -0,0 +1,10 @@
+class Vehiculo:
+    def __init__(self, ruedas:int, color:str) -> None:
+        self.ruedas = ruedas
+        self.color = color
+
+    def info(self):
+        print(f"El vehículo tiene {self.ruedas} ruedas y es de color {self.color}")
+
+
+
