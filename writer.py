2d44b8a64329fb72f2e44e87e2801aefe61c670d
alvarikola <alvaroe484@gmail.com>
2024-03-15 09:54:10 +0100

    Primera version writer.py

--- writer.py
+++ writer.py
@@ -1,11 +1,29 @@
-class Writer:
-    def __init__(self, file:str):
-        self.file = file
-
+class FileWriter:
+    def __init__(self, filename:str):
+        self.filename = filename
+        self.i = open(self.filename, "w+r")
 
     def write(self, mensaje:str):
-        pass
+        self.i.write(mensaje)
 
 
-    def read(self):
-        pass
+    def read(self)->str:
+        return self.i.readlines()
+
+    
+    def close(self):
+        self.i.close()
+
+
+
+def main():
+    prueba = FileWriter("hola.txt")
+    prueba.write("hola buenos dias")
+    prueba.close()
+    prueba = FileWriter("hola.txt")
+    prueba.read()
+    prueba.close()
+
+
+
+main()
